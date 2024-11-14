9751ecb554346f7bacb2a53095715f0a1a5dac48
Sathya-work <sathyanarayana.work@gmail.com>
2024-11-13 19:58:26 -0600

    made more changes

--- controllers/inventions.js
+++ controllers/inventions.js
@@ -14,17 +14,17 @@ exports.invention_detail = function(req, res) {
 };
 
 exports.invention_create_post = async function(req, res) {
-  let document = new Invention();
-  document.name = req.body.name;
-  document.inventor = req.body.inventor;
-  document.year = req.body.year;
-  try {
-    let result = await document.save();
-    res.send(result);
-  } catch (err) {
-    res.status(500);
-    res.send(`{"error": ${err}}`);
-  }
+    let document = new Invention();
+    document.name = req.body.name;
+    document.inventor = req.body.inventor;
+    document.year = req.body.year;
+    try {
+        let result = await document.save();
+        res.send(result);
+    } catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 };
 
 
@@ -34,4 +34,51 @@ exports.invention_delete = function(req, res) {
 
 exports.invention_update_put = function(req, res) {
   res.send('NOT IMPLEMENTED: Invention update PUT ' + req.params.id);
+};
+
+exports.invention_detail = async function(req, res) {
+  try {
+      const invention = await Invention.findById(req.params.id);
+      if (!invention) {
+          res.status(404).send('Invention not found');
+          return;
+      }
+      res.render('invention_detail', { invention });
+  } catch (err) {
+      res.status(500).send(`Error: ${err}`);
+  }
+};
+
+exports.invention_update_get = async function(req, res) {
+  try {
+      const invention = await Invention.findById(req.params.id);
+      if (!invention) {
+          res.status(404).send('Invention not found');
+          return;
+      }
+      res.render('invention_update', { invention });
+  } catch (err) {
+      res.status(500).send(`Error: ${err}`);
+  }
+};
+
+exports.invention_delete_get = async function(req, res) {
+  try {
+      const invention = await Invention.findById(req.params.id);
+      if (!invention) {
+          res.status(404).send('Invention not found');
+          return;
+      }
+      res.render('invention_delete', { invention });
+  } catch (err) {
+      res.status(500).send(`Error: ${err}`);
+  }
+};
+
+exports.invention_update_post = async function(req, res) {
+  res.send('NOT IMPLEMENTED: Invention update POST ' + req.params.id);
+};
+
+exports.invention_delete_post = async function(req, res) {
+  res.send('NOT IMPLEMENTED: Invention delete POST ' + req.params.id);
 };
