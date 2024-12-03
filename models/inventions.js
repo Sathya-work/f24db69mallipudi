09324497b05f5d72c4c41f9beab86c7cbef7fd68
Sathya-work <sathyanarayana.work@gmail.com>
2024-12-02 22:52:13 -0600

    Added new buttons

--- models/inventions.js
+++ models/inventions.js
@@ -2,7 +2,12 @@ const mongoose = require("mongoose");
 
 const inventionSchema = mongoose.Schema({
   name: String,
-  year: Number,
+  year: {
+    type: Number,
+    required: true,
+    min: [1000, 'Year must be at least 1000'],
+    max: [2024, 'Year must not exceed 2024']
+  },
   inventor: String
 });
 
