c3148d6cfff514b386b8ae33a331630e8290cc40
Sathya-work <sathyanarayana.work@gmail.com>
2024-12-02 22:08:14 -0600

    Added passport and login functionality

--- app.js
+++ app.js
@@ -30,6 +30,26 @@ const resourceRouter = require('./routes/resource');
 const inventionsRouter = require('./routes/inventions');
 const LocalStrategy = require('passport-local').Strategy;
 
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    Account.findOne({ username: username })
+      .then(function (user) {
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      })
+      .catch(function (err) {
+        return done(err)
+      })
+  })
+)
+
+
 // Schema definition
 const inventionSchema = new mongoose.Schema({
   name: String,
@@ -71,25 +91,6 @@ if (reseed) {
   recreateDB();
 }
 
-passport.use(new LocalStrategy(
-  function (username, password, done) {
-    Account.findOne({ username: username })
-      .then(function (user) {
-        if (err) { return done(err); }
-        if (!user) {
-          return done(null, false, { message: 'Incorrect username.' });
-        }
-        if (!user.validPassword(password)) {
-          return done(null, false, { message: 'Incorrect password.' });
-        }
-        return done(null, user);
-      })
-      .catch(function (err) {
-        return done(err)
-      })
-  })
-)
-
 // Express app setup
 const app = express();
 
@@ -102,6 +103,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+  }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
@@ -112,13 +122,10 @@ app.use('/grid', gridRouter);
 app.use('/selector', pickRouter);
 app.use('/resource', resourceRouter);
 
-app.use(require('express-session')({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: false
-  }));
-app.use(passport.initialize());
-app.use(passport.session());
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 // Error handling
 app.use(function (req, res, next) {
@@ -131,10 +138,5 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-var Account =require('./models/account');
-passport.use(new LocalStrategy(Account.authenticate()));
-passport.serializeUser(Account.serializeUser());
-passport.deserializeUser(Account.deserializeUser());
 
 module.exports = app;
