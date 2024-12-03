4f929f342fafe28656f0a589bf8ac4a9a7c5b6a0
Sathya-work <sathyanarayana.work@gmail.com>
2024-12-02 22:36:47 -0600

    Added login for update page

--- routes/inventions.js
+++ routes/inventions.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var invention_controller = require('../controllers/inventions');
+const passport = require('passport');
+
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    res.redirect("/login");
+    }
 
 router.get('/', invention_controller.invention_list);
 
@@ -20,8 +28,13 @@ router.get('/detail', invention_controller.invention_view_one_Page);
 
 router.get('/create', invention_controller.invention_create_Page);
 
-router.get('/update', invention_controller.invention_update_Page);
+router.get('/update', secured, invention_controller.invention_update_Page);
 
 router.get('/delete', invention_controller.invention_delete_Page);
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+});
+
 
 module.exports = router;
